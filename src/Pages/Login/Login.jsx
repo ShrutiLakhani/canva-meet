a49ae8fd017771e6f792e7b9c6d18da4f5257a45
ShrutiLakhani <82932172+ShrutiLakhani@users.noreply.github.com>
2022-08-01 16:41:33 +0530

    Added Home&Login

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import Navbar from "../../Components/components";
+import { userLogin } from "../../redux/features/authSlice";
+
+import "./Login.css";
+
+function Login() {
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { token } = useSelector((state) => state.auth);
+  const [loginData, setLoginData] = useState({ username: "", password: "" });
+  const testUser = {
+    username: "batmanmarvel@gmail.com",
+    password: "batmanmarvel",
+  };
+
+  const guestLogin = (e) => {
+    console.log("Comes here");
+    e.preventDefault();
+    userLogin(testUser);
+  };
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if (!loginData.password || !loginData.username) {
+      console.log("Fill complete form");
+    } else {
+      dispatch(userLogin(loginData));
+      console.log("Ready for dispatch");
+    }
+  };
+
+  useEffect(() => {
+    if (token && location.pathname === "/") {
+      navigate("/home");
+    }
+  }, [token, navigate, location]);
+  return (
+    <>
+      <main className="login-wrapper">
+        <section className="login-page-bottom-container">
+          <div className="login-container">
+            <form className="login-form">
+              <h1>Login</h1>
+              <div>
+                <label className="input-label">Email address</label>
+                <input
+                  className="form-input-container"
+                  type="text"
+                  placeholder="xyz@gmail.com"
+                  value={loginData.email}
+                  onChange={(e) =>
+                    setLoginData({ ...loginData, username: e.target.value })
+                  }
+                />
+              </div>
+              <div>
+                <label className="input-label">Password</label>
+                <input
+                  className="form-input-container"
+                  type="password"
+                  placeholder="**********"
+                  value={loginData.password}
+                  onChange={(e) =>
+                    setLoginData({ ...loginData, password: e.target.value })
+                  }
+                />
+              </div>
+              <div className="forgot-pswrd-link">
+                <a href="#">Forgot Password?</a>
+                <input className="style-input-checkbox" type="checkbox" />
+                Remember me
+              </div>
+              <button
+                className="button-login-form border-style"
+                onClick={(e) => handleLogin(e)}
+              >
+                LOGIN
+              </button>
+              <button
+                className="button-login-form border-style"
+                onClick={(e) => {
+                  guestLogin(e);
+                }}
+              >
+                GUEST LOGIN
+              </button>
+              <Link to="/signup" className="btn-sign-up button-link">
+                Create New Account <i class="fas fa-chevron-right"></i>
+              </Link>
+            </form>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
+export { Login };
